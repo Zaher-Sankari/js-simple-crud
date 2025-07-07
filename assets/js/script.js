a4ccb8a3918ac7ec2d61041ba91c7692cb5eb691
Zaher-Sankari <zaher.lp@gmail.com>
2025-07-07 22:18:18 +0300

    fixed delete and added counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,29 @@ const taskTitle = document.querySelector("#taskForm #taskTitle")
 console.log(container)
 
 let tasks = [{
+    count:1,
     id: 1,
     title: "frontend Task",
     status:true
 },
 {
+    count:2,
     id: 2,
     title: "backend Task",
     status:false
     }
 ]
+let lastId = tasks[tasks.length-1].id
+console.log(lastId);
 
 //Read Function
 const read = () =>{
     container.innerHTML=""
-    tasks.forEach((task) => {
+    tasks.forEach((task,index) => {
+        task.count = index +1
         container.innerHTML += `
             <tr class = "${task.status? "completed" : ""}">
+                <td>${task.count}</td>
                 <td>${task.id}</td>
                 <td>${task.title}</td>
                 <td><button onclick="editStatus(${task.id})">edit status</button><button onclick="editTitle('${task.id}')">edit title</button><button onclick="deleteTask(${task.id})">delete</button></td>
@@ -32,13 +38,21 @@ const read = () =>{
 //Write Function
 const add = (e) => {
     e.preventDefault();
-    let task = {
-        id:tasks[tasks.length-1].id +1,
-        title:taskTitle.value,
-        status:false
+    if(taskTitle.value != ""){
+        let task = {
+            count:tasks.length +1,
+            id:lastId+1,
+            title:taskTitle.value,
+            status:false
+        }
+        tasks.push(task)
+        taskTitle.value = ""
+        lastId++;
+        console.log(lastId);
     }
-    tasks.push(task)
-    taskTitle.value = ""
+    else{
+        alert("Please Insert a Valid title")
+    }
     read()
 }
 
@@ -53,7 +67,7 @@ const editStatus = (id) =>{
         }
         return task
     })
-   read()
+    read()
 }
 
 //Modify title
@@ -65,7 +79,7 @@ const editTitle = (id) =>{
         }
         return task
     })
-   read()
+    read()
 }
 
 //Delete Task:
